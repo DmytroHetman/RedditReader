302cc210265d07d8897bd198389f6670d35f5829
Hetman Dmytro <hetmandmitriy@gmail.com>
2022-02-15 22:02:55 +0200

    Initial Commit

---
+++ MyFirstApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApplication
+//
+//  Created by Dmytro Hetman on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
